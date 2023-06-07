6136c3ca212559f52602315912b46b99fe09eac7
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2023-06-07 14:58:41 +0300

    Updates

--- static/js/NotFoundView.3ba02384.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkeliftech_app=self.webpackChunkeliftech_app||[]).push([[558],{7954:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var a={heading:"NotFound_heading__penFw"},r=t(184);function u(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h2",{className:a.heading,children:"404 Page not found!"})})}function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(u,{})})}}}]);
-//# sourceMappingURL=NotFoundView.3ba02384.chunk.js.map
---
+++ static/js/NotFoundView.41a02bd1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkeliftech_app=self.webpackChunkeliftech_app||[]).push([[558],{7954:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var a={heading:"NotFound_heading__CnUZ-"},r=t(184);function u(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h2",{className:a.heading,children:"404 Page not found!"})})}function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(u,{})})}}}]);
+//# sourceMappingURL=NotFoundView.41a02bd1.chunk.js.map
